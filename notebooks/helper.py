6ddd717dcbed186234aa3c96ae2552c0e888e0e8
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-03 09:53:28 -0500

    overall update

--- notebooks/helper.py
+++ notebooks/helper.py
@@ -1,31 +1,28 @@
 import os
-from dotenv import load_dotenv, find_dotenv     
+from dotenv import load_dotenv, find_dotenv
 
 from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
 
-
-
 api_key = None
-dlai_endpoint = None
 client = None
+
 
 def load_env():
     _ = load_dotenv(find_dotenv())
+
 
 def load_mistral_api_key(ret_key=False):
     load_env()
     global api_key
     api_key = os.getenv("MISTRAL_API_KEY")
-
     global client
     client = MistralClient(api_key=api_key)
-    
     if ret_key:
         return api_key
 
 def mistral(user_message, 
-            model="open-mistral-7b",
+            model="mistral-small-latest",
             is_json=False):
     client = MistralClient(api_key=api_key)
     messages = [ChatMessage(role="user", content=user_message)]
@@ -39,5 +36,5 @@ def mistral(user_message,
         chat_response = client.chat(
             model=model, 
             messages=messages)
-
+        
     return chat_response.choices[0].message.content
