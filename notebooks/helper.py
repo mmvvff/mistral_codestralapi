524b403399a7b5efc81de0341c73265dd4a9fd56
Manuel Valderrama-Florez <valderrama.manuel@gmail.com>
2024-07-02 19:21:59 -0500

    update draft

--- notebooks/helper.py
+++ notebooks/helper.py
@@ -16,20 +16,18 @@ def load_env():
 def load_mistral_api_key(ret_key=False):
     load_env()
     global api_key
-    global dlai_endpoint
     api_key = os.getenv("MISTRAL_API_KEY")
-    dlai_endpoint = os.getenv("DLAI_MISTRAL_API_ENDPOINT")
 
     global client
-    client = MistralClient(api_key=api_key, endpoint=dlai_endpoint)
+    client = MistralClient(api_key=api_key)
     
     if ret_key:
-        return api_key, dlai_endpoint
+        return api_key
 
 def mistral(user_message, 
-            model="mistral-small-latest",
+            model="open-mistral-7b",
             is_json=False):
-    client = MistralClient(api_key=api_key, endpoint=dlai_endpoint)
+    client = MistralClient(api_key=api_key)
     messages = [ChatMessage(role="user", content=user_message)]
 
     if is_json:
@@ -43,31 +41,3 @@ def mistral(user_message,
             messages=messages)
 
     return chat_response.choices[0].message.content
-
-
-def get_text_embedding(txt):
-    global client
-    embeddings_batch_response = client.embeddings(
-        model="mistral-embed", 
-        input=txt
-    )
-    return embeddings_batch_response.data[0].embedding
-
-import requests
-from bs4 import BeautifulSoup
-import re
-def get_web_article_text(url, file_save_name=None):
-
-    response = requests.get(url)
-    html_doc = response.text
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    
-    
-    tag = soup.find("div", re.compile("^prose--styled"))
-    text = tag.text
-    
-    if file_save_name:
-        f = open(file_save_name, "w")
-        f.write(text)
-        f.close()
-    return text
